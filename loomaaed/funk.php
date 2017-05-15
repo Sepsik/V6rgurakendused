0b83cf55546bd6c0598314d8ce7be87c56ef274f
Anita Sepp <anitasepp@hotmail.com>
2017-05-15 15:14:26 +0300

    13 nädal lahendus lisatud

--- loomaaed/funk.php
+++ loomaaed/funk.php
@@ -13,8 +13,44 @@ function connect_db(){
 
 function logi(){
 	// siia on vaja funktsionaalsust (13. nädalal)
+	global $connection;
+	$errors = array(); 
 
-	include_once('views/login.html');
+	if(isset($_SESSION['user'])) {
+    	header('Location: loomaaed.php');
+    }
+	else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+		$username = mysqli_real_escape_string($connection, $_REQUEST["user"]);
+		$pwd = mysqli_real_escape_string($connection, $_REQUEST["pass"]);
+		
+		if (empty($username)){
+			$errors[]="Kasutajanimi on puudu";
+		}
+		if (empty($pwd)) {
+			$errors[]="Salasõna on puudu";
+		}
+		if (empty($errors)) {
+            //uus if, check against db
+			//kui k6ik on ok, siis session.user=1 ja return;
+			$userQuery = "SELECT * FROM anita_kylastajad WHERE username='$username' AND passw=SHA1('$pwd')";
+			$userResult = mysqli_query($connection, $userQuery) or die ("$userQuery - ". mysqli_error($connection));
+			if (mysqli_num_rows($userResult) != 0){
+				$_SESSION['user'] = 1;
+    			header('Location: loomaaed.php');
+			}
+			else {
+				echo "You don't exist";
+				include_once('views/login.html');
+			}
+			return;
+		}
+		//siia kui erroreid oli
+		include_once('views/login.html');
+	}
+	else {
+  	  include_once('views/login.html');
+	}
+
 }
 
 function logout(){
@@ -25,19 +61,25 @@ function logout(){
 
 function kuva_puurid(){
 	// siia on vaja funktsionaalsust
-	global $connection;
-	$puurid = array();
-	$query = "SELECT DISTINCT(puur) FROM anita_loomaaed";
-	$result = mysqli_query($connection, $query) or die ("$query - ". mysqli_error($connection));
-	while($rida = mysqli_fetch_assoc($result)){
-        $puuriId = $rida['puur'];
-    	$puuriQuery = "SELECT * FROM anita_loomaaed WHERE puur=$puuriId";
-    	$puuriResult = mysqli_query($connection, $puuriQuery) or die ("$puuriQuery - ". mysqli_error($connection));
-        while($puuriLoom = mysqli_fetch_assoc($puuriResult)){ 
-            $puurid[$puuriId][] = $puuriLoom;
+	if(isset($_SESSION['user'])) {
+		global $connection;
+		$puurid = array();
+		$query = "SELECT DISTINCT(puur) FROM anita_loomaaed ORDER BY puur";
+		$result = mysqli_query($connection, $query) or die ("$query - ". mysqli_error($connection));
+		while($rida = mysqli_fetch_assoc($result)){
+	        $puuriId = $rida['puur'];
+	    	$puuriQuery = "SELECT * FROM anita_loomaaed WHERE puur=$puuriId";
+	    	$puuriResult = mysqli_query($connection, $puuriQuery) or die ("$puuriQuery - ". mysqli_error($connection));
+	        while($puuriLoom = mysqli_fetch_assoc($puuriResult)){ 
+            	$puurid[$puuriId][] = $puuriLoom;
         }
 	}
-	include_once('views/puurid.html');
+		include_once('views/puurid.html');
+	}
+
+	else {
+		include_once('views/login.html');
+	}
 	/*
 	echo "<pre>";
 	print_r($puurid);
@@ -47,15 +89,47 @@ function kuva_puurid(){
 
 function lisa(){
 	// siia on vaja funktsionaalsust (13. nädalal)
-	
-	include_once('views/loomavorm.html');
-	
+	global $connection;
+	$errors = array(); 
+
+	if(isset($_SESSION['user'])) {
+		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+			$loomaNimi = mysqli_real_escape_string($connection, $_REQUEST["nimi"]);
+			$puuriNumber = mysqli_real_escape_string($connection, $_REQUEST["puur"]);
+			$liik = mysqli_real_escape_string($connection, upload('liik'));
+			if (empty($loomaNimi)){
+				$errors[]="Loomanimi on puudu";
+			}
+			if (empty($puuriNumber)) {
+				$errors[]="Puuri number on puudu";
+			}
+			if (empty($liik)) {
+				$errors[] = "Faili üleslaadimine ebaõnnestus";
+			}
+
+			if (empty($errors)) {
+				$animalQuery = "INSERT INTO anita_loomaaed (nimi, liik, puur) VALUES ('$loomaNimi', '$liik', $puuriNumber)";
+				$animalResult = mysqli_query($connection, $animalQuery) or die ("$animalQuery - ". mysqli_error($connection));
+					if (mysqli_insert_id($connection) > 0) {
+						kuva_puurid();
+						return;
+					}
+			}
+
+		}
+    	
+		include_once('views/loomavorm.html');
+    }
+    else {
+    	include_once('views/login.html');
+    }
 }
 
 function upload($name){
 	$allowedExts = array("jpg", "jpeg", "gif", "png");
 	$allowedTypes = array("image/gif", "image/jpeg", "image/png","image/pjpeg");
-	$extension = end(explode(".", $_FILES[$name]["name"]));
+	$tmp = explode(".", $_FILES[$name]["name"]);
+	$extension = end($tmp);
 
 	if ( in_array($_FILES[$name]["type"], $allowedTypes)
 		&& ($_FILES[$name]["size"] < 100000)
